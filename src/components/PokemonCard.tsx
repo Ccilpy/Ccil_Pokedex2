602769c4d7e1ae8eb9a72061c42bc2eabab24876
Ccilpy <cecilepuyol@gmail.com>
2024-10-22 16:45:29 +0200

    add interface props

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,18 +1,20 @@
-function PokemonCard({ pokemon }) {
-
-	return pokemon.imgSrc ? (
-		<figure>
-			<img src={pokemon.imgSrc} alt={pokemon.name} />
-			<figcaption> {pokemon.name} </figcaption>
-		</figure>
-	) : (
-		<>
-			<p>???</p>
-			<figcaption> {pokemon.name} </figcaption>
-		</>
-	);
+interface pokemon {
+  name: "string";
+  imgSrc?: "string";
 }
 
-
+function PokemonCard({ pokemon }) {
+  return pokemon.imgSrc ? (
+    <figure>
+      <img src={pokemon.imgSrc} alt={pokemon.name} />
+      <figcaption> {pokemon.name} </figcaption>
+    </figure>
+  ) : (
+    <>
+      <p>???</p>
+      <figcaption> {pokemon.name} </figcaption>
+    </>
+  );
+}
 
 export default PokemonCard;
