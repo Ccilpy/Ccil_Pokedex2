e3a722910480aec4c92f7def6e2087c2d327c64e
Ccilpy <cecilepuyol@gmail.com>
2024-10-22 13:39:34 +0200

    update Pokemon Card

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,27 @@
 function PokemonCard () {
-    return <figure> 
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" /> 
-        <figcaption>Bulbasaur</figcaption>
-        </figure>;
+    const pokemon = pokemonList[1];
+
+    return pokemon.imgSrc ? (
+        <figure> 
+        <img src= {pokemon.imgSrc} alt={pokemon.name} /> 
+        <figcaption> {pokemon.name} </figcaption>
+        </figure>
+    ) : (
+        <><p>???</p><figcaption> {pokemon.name} </figcaption></>
+    );
 }
+
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 
 export default PokemonCard
